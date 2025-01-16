5ee05e3e248985a0045fa4fb6d900b810c31a61b
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-17 00:26:31 +0900

    config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false  # アセットは事前にプリコンパイルする
+  config.assets.compile = true
   config.assets.digest = true    # アセットにダイジェストを追加
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
