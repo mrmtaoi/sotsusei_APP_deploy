a0c206ca8bc2fe499fa034b3c7cc780ec5c6c094
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-17 01:23:24 +0900

    take2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true    # アセットにダイジェストを追加
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true  # HTTPSを強制する設定（必要なら）
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.version = "1.0"
 # config/initializers/assets.rb
 
 # config/initializers/assets.rb
-Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w( application.scss )
