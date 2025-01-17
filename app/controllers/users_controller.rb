968f7f885fb66967bd6983dc3a0743ff08e93a6c
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-18 05:00:21 +0900

    take2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
     if @user.save
       # ユーザーを保存後に認証メールを送信
       @user.send_confirmation_instructions
@@ -16,16 +15,10 @@ class UsersController < ApplicationController
   end
   
   private
-      def set_user
-        @user = User.find(params[:id])
-      end
   
-      def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
+  def set_user
+    @user = User.find(params[:id])
   end
-
-  private
 
   def user_params
     params.require(:user).permit(:user_name, :email, :password)
