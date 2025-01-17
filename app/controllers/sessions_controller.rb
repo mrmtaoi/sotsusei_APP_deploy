ecfe6febafc606e2994fa39d68cf827e6dd3ae8e
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-18 04:47:46 +0900

    end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,26 @@
 class SessionsController < ApplicationController
 
-# app/helpers/sessions_helper.rb
-module SessionsHelper
-  # ユーザーをログインさせる
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  # app/helpers/sessions_helper.rb
+  module SessionsHelper
+    # ユーザーをログインさせる
+    def log_in(user)
+      session[:user_id] = user.id
+    end
 
-  # 現在ログインしているユーザーを取得
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+    # 現在ログインしているユーザーを取得
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
 
-  # ユーザーがログインしているかどうか
-  def logged_in?
-    !current_user.nil?
-  end
+    # ユーザーがログインしているかどうか
+    def logged_in?
+      !current_user.nil?
+    end
 
-  # ユーザーをログアウトさせる
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+    # ユーザーをログアウトさせる
+    def log_out
+      session.delete(:user_id)
+      @current_user = nil
+    end
   end
 end
