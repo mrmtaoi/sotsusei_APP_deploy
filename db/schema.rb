8b9f0962e1cf08a34bc36dccabf0305c52f244dd
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-17 22:15:17 +0900

    usersテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_16_193419) do
     t.datetime "activated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
