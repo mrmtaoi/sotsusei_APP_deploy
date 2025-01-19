d936b3d78cbaa68fd882ca3da2f9b69a676309c8
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-01-20 00:39:09 +0900

    kit_items_controller追加

---
+++ app/controllers/stocks/kit_items_controller.rb
@@ -0,0 +1,51 @@
+class Stocks::KitItemsController < ApplicationController
+  before_action :set_emergency_kit
+  before_action :set_kit_item, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @kit_items = @emergency_kit.kit_items
+  end
+
+  def create
+    @kit_item = @emergency_kit.kit_items.build(kit_item_params)
+    if @kit_item.save
+      redirect_to stocks_emergency_kit_path(@emergency_kit), notice: 'Kit item was successfully added.'
+    else
+      redirect_to stocks_emergency_kit_path(@emergency_kit), alert: 'Failed to add kit item.'
+    end
+  end
+  
+  def edit
+  end  
+  
+  def update
+    if @kit_item.update(kit_item_params)
+      redirect_to stocks_emergency_kit_path(@kit_item.emergency_kit), notice: 'アイテムが更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @kit_item.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@kit_item) }
+      format.html { redirect_to stocks_emergency_kit_path(@kit_item.emergency_kit), notice: "Item was successfully destroyed." }
+    end
+  end
+  
+  
+  private
+  
+  def set_emergency_kit
+    @emergency_kit = EmergencyKit.find(params[:emergency_kit_id])
+  end
+  
+  def set_kit_item
+    @kit_item = @emergency_kit.kit_items.find(params[:id])
+  end
+  
+  def kit_item_params
+    params.require(:kit_item).permit(:name, :quantity, reminders_attributes: [:expiration_date])
+  end
+end
