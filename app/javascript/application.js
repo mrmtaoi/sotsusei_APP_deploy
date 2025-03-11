83458c9d8c3e62cb30ddccf8f1f5ce8a05a9be16
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-03-12 01:46:09 +0900

    エラー修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
-import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";  // 必要に応じて
-
-// Turbo
-import "@hotwired/turbo-rails";
-
-// Stimulus application のセットアップ
-const application = Application.start();
-
-// コントローラーの自動登録
-const context = require.context("controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
+// app/assets/javascripts/application.js
+import "@hotwired/turbo-rails"
+import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,7 @@
+# Pin npm packages by running ./bin/importmap
+
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-
-# Stimulus コントローラーを自動読み込み
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-pin "category_controller", to: "controllers/category_controller.js"
