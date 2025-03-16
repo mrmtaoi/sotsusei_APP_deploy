63ec509c8fe71e4deec7bc2a91a30f9ce0f8fd49
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-03-17 01:08:55 +0900

    take3

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,7 @@
-// app/assets/config/manifest.js
 //= link_tree ../images
 //= link application.js
 //= link application.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link @hotwired--stimulus.js
-//= link autocomplete_controller.js
+//= link @hotwired/stimulus.js
+//= link_tree ../../javascript/controllers .js
