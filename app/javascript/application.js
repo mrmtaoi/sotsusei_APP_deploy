1b42b63b3064917c281978b56d85a8233f03a324
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-03-17 21:16:55 +0900

    take7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,9 @@
-// app/javascript/application.js
-import { Application } from "stimulus"  // stimulusはimportmapでpinしているので、そのまま使う
-import { definitionsFromContext } from "stimulus/webpack-helpers"  // Stimulusのwebpack-helpers
-
-import { AutocompleteController } from "./controllers/autocomplete_controller" // 自作のコントローラーをインポート
+import { Application } from "@hotwired/stimulus"
+import AutocompleteController from "./controllers/autocomplete_controller" // パスを確認
 
 const application = Application.start()
-
-// コントローラーを登録
 application.register("autocomplete", AutocompleteController)
 
-// 他のコントローラーのインポート
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
-
-// Configure Stimulus development experience
-application.debug = false
 window.Stimulus = application
 
 export { application }
