58d21669f33a7e55ce18a5443305a6b5ca5cc828
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-03-20 02:31:55 +0900

    カテゴリ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# db/seeds.rb
-# 保持するカテゴリのリスト
-allowed_categories = ["食料", "飲料", "医療・衛生用品", "トイレ関係", "防寒具", "寝具", "衣類", "乾電池", "ライト", "ラジオ", "スマホ充電機器", "その他"]
+# 既存データの削除（削除順に注意）
+Reminder.delete_all  # まず reminders を削除
+KitItem.delete_all   # 次に kit_items を削除
+Category.delete_all  # 最後に categories を削除
 
-# 現在のカテゴリのうち、指定されていないカテゴリを削除
-Category.where.not(name: allowed_categories).destroy_all
+# カテゴリーの登録
+allowed_categories = ["食料", "飲料", "医療・衛生用品", "防寒具", "寝具", "衣類", "乾電池", "ライト", "ラジオ", "スマホ充電機器", "トイレ用品", "貴重品・身分証明書関係", "その他"]
 
 # 新しいカテゴリの作成（重複しないようにする）
 allowed_categories.each do |category|
-  Category.find_or_create_by(name: category)
+  Category.find_or_create_by!(name: category)
 end
