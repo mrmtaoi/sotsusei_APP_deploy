3ec471360274be201251e00c0d0fd8fbc83607ea
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-04-05 15:34:41 +0900

    expiration_dateを空欄で登録してもエラーが発生しないようにkit_itemモデルにaccepts_nested_attributes_for に reject_if オプションを追加

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,4 +3,10 @@ class Reminder < ApplicationRecord
   belongs_to :emergency_kit, optional: true
   belongs_to :stock_item, optional: true
   belongs_to :user, optional: true
+
+  private
+
+  def set_user_id_from_kit_item
+    self.user_id ||= kit_item&.emergency_kit&.user_id
+  end
 end
