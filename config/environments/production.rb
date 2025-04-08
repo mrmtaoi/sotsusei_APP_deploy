27d3eb5bb89b8a15875103c97cf14e02c4ce25d9
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-04-09 01:17:21 +0900

    take2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://sotsusei-app-deploy.onrender.com/' }
-
+  config.action_mailer.default_url_options = { host: 'https://sotsusei-app-deploy-4.onrender.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
