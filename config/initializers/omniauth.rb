d83555a2588abb39eddb5d2c79479fcc638a94dd
森本葵 <aoi@morimotoaoinoMacBook-Air.local>
2025-05-02 02:34:25 +0900

    調整２

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,7 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-    {
-    scope: 'email,profile', 
-    prompt: 'select_account',
-    redirect_uri: 'https://stock-supporter2025.com/auth/google_oauth2/callback'
-    }
+    provider :google_oauth2, 
+             ENV['GOOGLE_CLIENT_ID'], 
+             ENV['GOOGLE_CLIENT_SECRET'],
+             redirect_uri: 'https://stock-supporter2025.com/auth/google_oauth2/callback'
   end
-  
