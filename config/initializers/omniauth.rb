ea9e6449bfb6a13c44fa13f30a5b6d4bc854e23b
mrmtaoi <morimoaoi0518@gmail.com>
2025-05-20 01:27:58 +0900

    omniauth.rb修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,10 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-    ENV['GOOGLE_CLIENT_ID'],
-    ENV['GOOGLE_CLIENT_SECRET'],
-    {
-      scope: 'email,profile',
-      prompt: 'select_account',
-      provider_ignores_state: false
-    }
-end  
+    provider :google_oauth2,
+             ENV['GOOGLE_CLIENT_ID'],
+             ENV['GOOGLE_CLIENT_SECRET'],
+             scope: 'email,profile',
+             redirect_uri: 'https://stock-supporter2025.com/auth/google_oauth2/callback',
+             provider_ignores_state: true
+  end
+  
