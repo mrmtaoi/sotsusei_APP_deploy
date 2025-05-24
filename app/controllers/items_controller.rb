c9fb7fb9a958c1b27efc37f9b543997fdf96db04
mrmtaoi <morimoaoi0518@gmail.com>
2025-05-25 04:29:14 +0900

    アイテム検索を2文字以上で入力するよう変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
   def search
     keyword = params[:keyword]
-    @items = if keyword.present?
-               RakutenWebService::Ichiba::Item.search(keyword: keyword)
-             else
-               []
-             end
+    if keyword.present? && keyword.length >= 2
+      @items = RakutenWebService::Ichiba::Item.search(keyword: keyword)
+      @error_message = nil
+    else
+      @items = []
+      @error_message = "検索ワードは2文字以上で入力してください"
+    end
   end
 end
