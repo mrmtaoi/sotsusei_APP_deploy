b9084e78d3b3f8af14731ef9962c7c777e033268
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-03 01:30:30 +0900

    omniauth.rb修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,13 +3,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
     ENV['GOOGLE_CLIENT_ID'],
     ENV['GOOGLE_CLIENT_SECRET'],
-      {
-       scope: 'openid email profile',
-       redirect_uri: 'https://stock-supporter2025.com/auth/google_oauth2/callback',
-       access_type: 'offline',
-       prompt: 'consent',
-       select_account: true,
-       provider_ignores_state: false
-      }
-  end
+    {
+      scope: 'openid email profile',
+      redirect_uri: ENV['GOOGLE_REDIRECT_URI'],
+      prompt: 'consent select_account',
+      access_type: 'offline',
+      provider_ignores_state: false
+    }
+end
   
