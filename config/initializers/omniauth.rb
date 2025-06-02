96b796d46ec07261fa9d746c99bf675c263437be
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-02 20:40:21 +0900

    omniauth.rbをスコープとオプションを明示的に拡充

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,15 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2,
-             ENV['GOOGLE_CLIENT_ID'],
-             ENV['GOOGLE_CLIENT_SECRET'],
-             scope: 'email,profile',
-             redirect_uri: 'https://stock-supporter2025.com/auth/google_oauth2/callback',
-             provider_ignores_state: true
+  provider :google_oauth2,
+    ENV['GOOGLE_CLIENT_ID'],
+    ENV['GOOGLE_CLIENT_SECRET'],
+      {
+       scope: 'openid email profile',
+       redirect_uri: 'https://stock-supporter2025.com/auth/google_oauth2/callback',
+       access_type: 'offline',
+       prompt: 'consent',
+       select_account: true,
+       provider_ignores_state: false
+      }
   end
   
