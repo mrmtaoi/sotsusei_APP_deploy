5ba15a3e3f59000e136e724f82150b08144080ba
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-04 01:28:32 +0900

    session_store.rb修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # config/initializers/session_store.rb
-
 Rails.application.config.session_store :cookie_store,
   key: '_stock_supporter_session',
-  same_site: :lax,
-  secure: Rails.env.production?
+  same_site: :none,      # ← Google認証後のリダイレクト対応に必要
+  secure: true           # ← HTTPSが必須（Render本番環境はOK）
