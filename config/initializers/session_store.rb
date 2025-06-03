8350f90308fa5e9eb5dde4aac1846593e3d01e44
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-03 22:30:47 +0900

    session_store.rb修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,6 @@
 
 Rails.application.config.session_store :cookie_store,
   key: '_stock_supporter_session',
-  same_site: :lax,
-  secure: Rails.env.production? 
+  same_site: :none,
+  secure: true # `secure: Rails.env.production?` ではなく常に true に
+
