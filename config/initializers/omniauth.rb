f682c5d471c2982a266d750d6c0ed279620cfbd3
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-05 00:50:06 +0900

    omniauth.rbからリダイレクトURIの記載削除

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,14 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-    ENV['GOOGLE_CLIENT_ID'],
-    ENV['GOOGLE_CLIENT_SECRET'],
-    {
-      scope: 'openid email profile',
-      redirect_uri: ENV['GOOGLE_REDIRECT_URI'],
-      prompt: 'consent select_account',
-      access_type: 'offline',
-      provider_ignores_state: true
-    }
+  ENV['GOOGLE_CLIENT_ID'],
+  ENV['GOOGLE_CLIENT_SECRET'],
+  {
+    scope: 'openid email profile',
+    prompt: 'consent select_account',
+    access_type: 'offline',
+    provider_ignores_state: true
+  }
 end
 OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
