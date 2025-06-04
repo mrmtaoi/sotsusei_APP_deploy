891482665806c6aaa8a125caaf67a31bab8aca1f
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-05 00:02:30 +0900

    session_store.rb修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # config/initializers/session_store.rb
+
 Rails.application.config.session_store :cookie_store,
   key: '_stock_supporter_session',
-  domain: Rails.env.production? ? '.stock-supporter2025.com' : nil,
   same_site: Rails.env.production? ? :none : :lax,
   secure: Rails.env.production?
-  
