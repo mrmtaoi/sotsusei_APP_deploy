c2d66ff8e5b0c72c9ff5427d564ef1f9d77eb711
mrmtaoi <morimoaoi0518@gmail.com>
2025-06-04 23:48:59 +0900

    omniauth.rb修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
     ENV['GOOGLE_CLIENT_ID'],
-    ENV['GOOGLE_CLIENT_SECRET'],More actions
+    ENV['GOOGLE_CLIENT_SECRET'],
     {
       scope: 'openid email profile',
       redirect_uri: ENV['GOOGLE_REDIRECT_URI'],
